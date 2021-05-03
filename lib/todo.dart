e7a267b1fa8eb9b1b92f2af28a12e462c319b219
yusame0308 <yu.starts.yu@gmail.com>
2021-05-03 23:34:20 +0900

    null safety (Todo class)

--- lib/todo.dart
+++ lib/todo.dart
@@ -2,13 +2,18 @@ import 'package:flutter/material.dart';
 
 @immutable
 class Todo {
-  const Todo({this.id, this.title, this.time, this.isDone, this.belong});
+  const Todo(
+      {required this.id,
+      required this.title,
+      this.time,
+      this.isDone = false,
+      this.belong = Belong.None});
 
-  final int id;
-  final String title;
-  final int time;
-  final bool isDone;
-  final Belong belong;
+  final int id; //一意のid
+  final String title; //タイトル
+  final int? time; //所要時間(nullable)
+  final bool isDone; //完了したかどうか
+  final Belong belong; //やらない or 絶対やる or できればやる
 }
 
 enum Belong {
