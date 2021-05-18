90bc2c57232bbb636cf682f44454b2e099774ea3
yusame0308 <yu.starts.yu@gmail.com>
2021-05-18 15:04:55 +0900

    add Dismissible

--- lib/template_list_view.dart
+++ lib/template_list_view.dart
@@ -28,79 +28,82 @@ class TemplateListView extends HookWidget {
           final _todo = _todoList[index];
           return Card(
             elevation: 2,
-            child: ListTile(
-              title: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(_todo.title),
-                  Row(
-                    children: [
-                      Icon(
-                        Icons.timer_sharp,
-                        size: 14,
-                        color: Colors.teal[300],
-                      ),
-                      Text(
-                        '20',
-                        style: TextStyle(
+            child: Dismissible(
+              key: UniqueKey(),
+              child: ListTile(
+                title: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(_todo.title),
+                    Row(
+                      children: [
+                        Icon(
+                          Icons.timer_sharp,
+                          size: 14,
                           color: Colors.teal[300],
-                          fontSize: 14,
                         ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-              onTap: () {
-                transitionToNextScreen(context, Const.routeNameUpsertTodo,
-                    todo: _todo);
-              },
-              trailing: IconButton(
-                icon: Icon(Icons.add),
-                onPressed: () async {
-                  final result = await showDialog<Belong>(
-                    context: context,
-                    builder: (BuildContext context) {
-                      return AlertDialog(
-                        title: const Text('追加'),
-                        content: Text('${_todo.title}を明日やることに追加しますか？'),
-                        actions: [
-                          TextButton(
-                            onPressed: () {
-                              context
-                                  .read(todoViewModelProvider.notifier)
-                                  .updateTodo(
-                                      id: _todo.id,
-                                      title: _todo.title,
-                                      isDone: _todo.isDone,
-                                      belong: Belong.Sure);
-                              Navigator.pop(context, Belong.Sure);
-                            },
-                            child: const Text('絶対やる'),
+                        Text(
+                          '20',
+                          style: TextStyle(
+                            color: Colors.teal[300],
+                            fontSize: 14,
                           ),
-                          TextButton(
-                            onPressed: () {
-                              context
-                                  .read(todoViewModelProvider.notifier)
-                                  .updateTodo(
-                                      id: _todo.id,
-                                      title: _todo.title,
-                                      isDone: _todo.isDone,
-                                      belong: Belong.Unsure);
-                              Navigator.pop(context, Belong.Unsure);
-                            },
-                            child: const Text('できればやる'),
-                          ),
-                        ],
-                      );
-                    },
-                  );
-                  if (result == Belong.Sure) {
-                    Navigator.pop(context, '明日、${_todo.title}を絶対にやります');
-                  } else if (result == Belong.Unsure) {
-                    Navigator.pop(context, '明日、${_todo.title}をできればやります');
-                  }
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                onTap: () {
+                  transitionToNextScreen(context, Const.routeNameUpsertTodo,
+                      todo: _todo);
                 },
+                trailing: IconButton(
+                  icon: Icon(Icons.add),
+                  onPressed: () async {
+                    final result = await showDialog<Belong>(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return AlertDialog(
+                          title: const Text('追加'),
+                          content: Text('${_todo.title}を明日やることに追加しますか？'),
+                          actions: [
+                            TextButton(
+                              onPressed: () {
+                                context
+                                    .read(todoViewModelProvider.notifier)
+                                    .updateTodo(
+                                        id: _todo.id,
+                                        title: _todo.title,
+                                        isDone: _todo.isDone,
+                                        belong: Belong.Sure);
+                                Navigator.pop(context, Belong.Sure);
+                              },
+                              child: const Text('絶対やる'),
+                            ),
+                            TextButton(
+                              onPressed: () {
+                                context
+                                    .read(todoViewModelProvider.notifier)
+                                    .updateTodo(
+                                        id: _todo.id,
+                                        title: _todo.title,
+                                        isDone: _todo.isDone,
+                                        belong: Belong.Unsure);
+                                Navigator.pop(context, Belong.Unsure);
+                              },
+                              child: const Text('できればやる'),
+                            ),
+                          ],
+                        );
+                      },
+                    );
+                    if (result == Belong.Sure) {
+                      Navigator.pop(context, '明日、${_todo.title}を絶対にやります');
+                    } else if (result == Belong.Unsure) {
+                      Navigator.pop(context, '明日、${_todo.title}をできればやります');
+                    }
+                  },
+                ),
               ),
             ),
           );
